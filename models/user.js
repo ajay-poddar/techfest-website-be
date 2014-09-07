d3503fdc808935d3c5bb2b6bba4db0ffb04cadcb
zipzapzoom <ankit.technited@gmail.com>
2014-09-07 17:56:02 +0530

    User Api Fixed

--- models/user.js
+++ models/user.js
@@ -8,29 +8,36 @@ var User = function(){
 
   //Schema Defination
   userSchema = new Schema({
-  	"user-id" : {type:String, unique:true},
+  	"userId" : {type:String, unique:true},
   	"name" : String,
   	"email" : String,
-  	"event-id" : String,
-  	"user-type" : Boolean
+  	"eventId" : String,
+  	"userType" : Boolean
   });
 
   mongoose.connect('mongodb://localhost/techfest');
 
   _userModel = mongoose.model("users",userSchema);
 
-  _createUser = function(data,callback){
-  	console.log(data.body);
-  	_userModel.find({"user-id" : data.body.userid},function(err,obj){
-  		if(obj){
-  			console.log("User Exists!");
-  			callback(null,"User Already Exists!");
-  		}else{
-  			console.log("New User!");
-  			var user = new _userModel(data.body);
-  			user.save(callback);
-  		}
-  	});
+ _createUser = function(data,callback){
+    console.log(data.body);
+    _userModel.find({"userId" : data.body.userId},function(err,obj){
+      if(obj){
+        if(obj.length > 0)
+        {
+          console.log("User Exists!");
+          callback(null,"User Already Exists!");
+        }
+        else
+        {
+          console.log("New User!");
+          var user = new _userModel(data.body);
+          user.save(callback);
+        }
+      }else{
+        console.log("DB Error!");
+      }
+    });
   };
 
   return{
